54c5b71564aba7e52a011b38441075384bbe9e1a
Sandeep31 <sandeep92.avasarala@gmail.com>
2022-06-26 03:23:01 -0400

    cleaner approach without state

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -6,38 +6,23 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 
 export const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  const expenses = props.expenses.map((element) => {
-    return (
-      <ExpenseItem
-        key={Math.random()}
-        title={element.title}
-        amount={element.amount}
-        date={element.date}
-      />
-    );
-  });
-  const [filteredExpenses, setFilteredExpenses] = useState(expenses);
 
   const yearFilterHandler = (filter) => {
     setFilteredYear(filter);
-    setFilteredExpenses(getFilterExpenses(filter));
-    console.log("Inside Expenses", filter);
   };
 
-  const getFilterExpenses = (filter) => {
-    return props.expenses
-      .filter((expense) => expense.date.getFullYear() === parseInt(filter))
-      .map((element) => {
-        return (
-          <ExpenseItem
-            key={Math.random()}
-            title={element.title}
-            amount={element.amount}
-            date={element.date}
-          />
-        );
-      });
-  };
+  const filteredExpenses = props.expenses
+    .filter((expense) => expense.date.getFullYear().toString() === filteredYear)
+    .map((element) => {
+      return (
+        <ExpenseItem
+          key={Math.random()}
+          title={element.title}
+          amount={element.amount}
+          date={element.date}
+        />
+      );
+    });
 
   return (
     <div>
